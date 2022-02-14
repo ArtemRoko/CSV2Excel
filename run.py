5be28917502b2bc12d5c735a4caef26563f822d1
Artem Prokopenko <artem.prokopenko-non-empl@moodys.com>
2022-02-14 15:06:45 +0300

    feat: Using pool of 8 workers

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 from src.csv2excel import CSV2ExcelProcessor
+import time
 
 if __name__ == '__main__':
 
     # Set params
     csv_dir = './input_csv'  # Program will process every csv file in this dir
-    template_file = './template/CGV_enrichment_template_v2.xlsx'  # Should be in xlsx format
+    template_file = './template/CGV_enrichment_template_v4.0.xlsx'  # Should be in xlsx format
     sheet_name = 'Template'  # Where to copy data from CSVs
-    unprotected_columns_ids = [22, 23, 24, 26, 27, 28, 31]  # Columns that should be editable
-    columns_to_int = [2, 6, 17, 18, 19]  # IDs of columns in csv file, where decimal point should be deleted
+    unprotected_columns_ids = [22, 23, 24, 26, 27, 28, 29, 42]  # Columns that should be editable
+    columns_to_int = [2, 6, 17, 18, 19]  # IDs of columns in csv file that will be cast to int
     output_dir = './output_excel'  # output file name = [csv_name]_[template_name].xlsx
     skip_existing = True  # skip processing if an output file already exists
 
+    t1 = time.monotonic()
     CSV2ExcelProcessor.csv2template(csv_dir=csv_dir,
                                     template_path=template_file,
                                     output_dir=output_dir,
@@ -18,3 +20,6 @@ if __name__ == '__main__':
                                     columns_to_int=columns_to_int,
                                     sheet_name=sheet_name,
                                     skip_existing=skip_existing)
+    t2 = time.monotonic()
+    print(f'One file per {(t2-t1)/25:.2f} s')
+
--- src/csv2excel.py
+++ src/csv2excel.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from typing import List, Tuple
 import shutil
 from copy import copy
+from multiprocessing import Pool
+from functools import partial
 
 import pandas as pd
 from openpyxl.styles.protection import Protection
@@ -18,6 +20,16 @@ class CSV2ExcelProcessor:
         return files
 
     @staticmethod
+    def get_max_csv_cordinality(dir_path: str):
+        files = CSV2ExcelProcessor._get_files(dir_path)
+        max_rows = 0
+        for file in tqdm(files):
+            df = pd.read_csv(file, header=None)
+            if max_rows < df.shape[0]:
+                max_rows = df.shape[0]
+        return max_rows
+
+    @staticmethod
     def _replace_escapes(text: str) -> str:
         escapes = ''.join([chr(char) for char in range(1, 32)])
         translator = str.maketrans('', '', escapes)
@@ -30,7 +42,7 @@ class CSV2ExcelProcessor:
         csv_data.fillna('', inplace=True)
         csv_data = csv_data.applymap(CSV2ExcelProcessor._replace_escapes)
         for idx in columns_to_int:
-            csv_data[idx] = csv_data[idx].apply(lambda x: x.split('.')[0])
+            csv_data[idx] = pd.to_numeric(csv_data[idx].apply(lambda x: x.split('.')[0]))
         return csv_data
 
     @staticmethod
@@ -45,6 +57,8 @@ class CSV2ExcelProcessor:
         for i, row in enumerate(sheet.rows):
             if i < 2:
                 continue
+            elif i > 300:
+                break
             for j, cell in enumerate(row):
                 if i == 2:
                     if j in unprotected_col_ids:
@@ -93,35 +107,51 @@ class CSV2ExcelProcessor:
             return
 
         failed_files = []
+        files_tuple = []
+        for csv_file in csv_files:
 
-        for csv_file in tqdm(csv_files):
-            output_file_path = None
-            try:
-                skip, output_file_path = CSV2ExcelProcessor._prepare_template_copy(csv_file,
-                                                                                   template_path,
-                                                                                   output_dir,
-                                                                                   skip_existing)
-                if skip:
-                    print(f'{output_file_path} already exists. Skipping...')
-                    continue
+            skip, output_file_path = CSV2ExcelProcessor._prepare_template_copy(csv_file,
+                                                                               template_path,
+                                                                               output_dir,
+                                                                               skip_existing)
+            if skip:
+                print(f'{output_file_path} already exists. Skipping...')
+                continue
 
-                csv_df = CSV2ExcelProcessor._load_csv(csv_file, columns_to_int)
-                with pd.ExcelWriter(output_file_path, mode='a', if_sheet_exists='overlay') as excel_writer:
-                    csv_df.to_excel(excel_writer,
-                                    sheet_name=sheet_name,
-                                    index=False,
-                                    startcol=4,
-                                    startrow=2,
-                                    header=None)
-                    CSV2ExcelProcessor._restore_formatting(excel_writer, sheet_name, unprotected_col_ids)
+            files_tuple.append((csv_file, output_file_path))
+        f = partial(CSV2ExcelProcessor._process_one_file,
+                    unprotected_col_ids=unprotected_col_ids,
+                    columns_to_int=columns_to_int,
+                    sheet_name=sheet_name)
 
-            except Exception as e:
-                print(f"Couldn't process file: {csv_file}")
-                print(f'Exception details: {str(e)}')
-                if output_file_path is not None:
-                    Path(output_file_path).unlink(missing_ok=True)
-                    failed_files.append(csv_file)
+        with Pool(8) as p:
+            p.map(f, files_tuple)
 
         if len(failed_files) > 0:
             print('Processing done. Failed to process these files:')
             [print(file) for file in failed_files]
+
+    @staticmethod
+    def _process_one_file(input_output_files: Tuple[str, str],
+                          unprotected_col_ids: List[int],
+                          columns_to_int: List[int],
+                          sheet_name: str):
+        try:
+            csv_file, output_file_path = input_output_files
+            print(f'{csv_file} --> {output_file_path}')
+            csv_df = CSV2ExcelProcessor._load_csv(csv_file, columns_to_int)
+            with pd.ExcelWriter(output_file_path, mode='a', if_sheet_exists='overlay') as excel_writer:
+                csv_df.to_excel(excel_writer,
+                                sheet_name=sheet_name,
+                                index=False,
+                                startcol=4,
+                                startrow=2,
+                                header=None)
+                CSV2ExcelProcessor._restore_formatting(excel_writer, sheet_name, unprotected_col_ids)
+
+        except Exception as e:
+            print(f"Couldn't process file: {csv_file}")
+            print(f'Exception details: {str(e)}')
+            if output_file_path is not None:
+                Path(output_file_path).unlink(missing_ok=True)
+            raise EnvironmentError(f"Can't process {csv_file}")
